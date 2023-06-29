83f852216a8c51c1e2ed8b9a512645a461631a34
thanhthuongle <Thuong.LT215485@sis.hust.edu.vn>
2023-06-30 05:27:30 +0700

    test upload folder

---
+++ Main8/Controller.java
@@ -0,0 +1,18 @@
+package Main8;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+public class Controller{
+	@FXML
+	private TextField Height;
+	
+	public void Submit (ActionEvent event) {
+		String height = Height.getText();
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setContentText("chiều cao của bạn là: "+height+" cm");
+		alert.show();
+	}
+}
